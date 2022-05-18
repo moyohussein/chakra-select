b7bac64493c4693a8f32981918e9449e69abda92
Hussein AbdulQohar <aqmhussein@gmail.com>
2022-05-18 10:32:53 +0100

    chore: add pages folder

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import { Button } from '@chakra-ui/react'
+import React from 'react'
+
+const Homepage = () => {
+  return (
+    <div>
+        Homepage
+
+    </div>
+    
+  )
+}
+
+export default Homepage
