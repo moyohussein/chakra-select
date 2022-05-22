73762649dbbcc9797b2c35ea83db1d2841dfce61
Hussein AbdulQohar <aqmhussein@gmail.com>
2022-05-22 03:31:25 +0100

    chore: page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
-import { Text, BaseThemeTypings } from "@chakra-ui/react";
+import { Text, BaseThemeTypings, Container } from "@chakra-ui/react";
+import { useState } from "react";
 
 import Option from "../components/Option";
 import Select from "../components/Select";
@@ -6,10 +7,18 @@ import Select from "../components/Select";
 const Homepage = () => {
   return (
     <div>
-      <Text>Homepage</Text>
-      <Select placeholder="Select Option">
-        <Option label="shayo">Shayo</Option>
-      </Select>
+      <Container>
+        <Text>Chakrathon 2022 Select Component</Text>
+        <Select size="sm" variant="outline">
+          <Option value="a" label="Ayo">Ayo</Option>
+          <Option value="b" label="Bayo">Bayo</Option>
+          <Option value="c" label="Dayo">Dayo</Option>
+          <Option value="d" label="Layo">Layo</Option>
+          <Option value="e" label="Sayo">Sayo</Option>
+          <Option value="f" label="Tayo">Tayo</Option>
+          <Option value="g" label="Wayo">Wayo</Option>
+        </Select>
+      </Container>
     </div>
   );
 };
