3b3bb447ff730c560a0cd359edae1eec67006786
Hussein AbdulQohar <aqmhussein@gmail.com>
2022-05-22 03:30:47 +0100

    chore: remove unused hooks

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,6 +1,4 @@
 export * from "./useActiveElement";
-export * from "./useKeyPress";
 export * from "./useOnClickOutside";
 export * from "./useScrollLock";
 export * from "./useToggle";
-export * from "./useInterval";
