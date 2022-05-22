87f126b631b764946c76e670f078f8621b8bf481
Hussein AbdulQohar <aqmhussein@gmail.com>
2022-05-22 03:31:36 +0100

    feat: add theme

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,30 +1,119 @@
-import { extendTheme, theme as baseTheme } from "@chakra-ui/react";
+import type { ComponentStyleConfig } from '@chakra-ui/theme'
+import { extendTheme } from '@chakra-ui/react'
 
-type Dict = Record<string, any>;
-
-const customTheme = {
-  ...baseTheme,
-  components: {
-    ...baseTheme.components,
-    Button: {
-      baseStyle: baseTheme.components.Button.baseStyle,
-      defaultProps: baseTheme.components.Button.defaultProps,
-      sizes: {
-        md: baseTheme.components.Button.sizes.md,
+// You can also use the more specific type for
+// a multipart component: ComponentMultiStyleConfig
+const ChakraSelect: ComponentStyleConfig = {
+  // All parts of multipart components can be found in the @chakra-ui/anatomy package,
+  // the menuAnatomy has as well these parts: button, list, groupTitle, command, divider
+  parts: ['select', 'option'],
+  baseStyle: {
+    select: {
+      boxShadow: 'md',
+      rounded: 'lg',
+      alignItems: 'center',
+      px: '4',
+      width: 'max-content',
+      fontWeight: 'medium',
+      color: 'gray.700',
+      lineHeight: 'normal',
+      _focusWithin: {
+        boxShadow: '0 0 0 2px rgba(21, 156, 228, 0.4)'
       },
-      variants: {
-        primary: () => {
-          const defaultStyle = baseTheme.components.Button.variants.solid;
-          return {
-            ...defaultStyle,
-            bg: "blue.900",
-          };
-        },
+      _expanded: {
+        boxShadow: '0 0 0 2px rgba(21, 156, 228, 0.4)'
+      },
+      _focus: {
+        outline: 'none'
+      }
+    },
+    option: {
+      fontWeight: 'medium',
+      rounded: 'lg',
+      lineHeight: 'normal',
+      textAlign: 'left',
+      px: '4',
+      color: 'gray.500',
+      width: '100%',
+      _hover:{
+        backgroundColor: 'gray.100',
+      },
+      _focus: {
+        outline: 'none',
+        backgroundColor: 'gray.300',
+      },
+      
+    },
+  },
+  sizes: {
+    xs: {
+      select: {
+        fontSize: 'xs',
+        py: 1,
+      },
+      option: {
+        fontSize: 'xs',
+        py: 1,
+      },
+    },
+    sm: {
+      select: {
+        fontSize: 'sm',
+        py: 1.5,
+      },
+      option: {
+        fontSize: 'sm',
+        py: 1.5,
+      },
+    },
+    md: {
+      select: {
+        fontSize: 'md',
+        py: 2,
+      },
+      option: {
+        fontSize: 'md',
+        py: 2,
+      },
+    },
+    lg: {
+      select: {
+        fontSize: 'lg',
+        py: 3,
+      },
+      option: {
+        fontSize: 'lg',
+        py: 3,
       },
     },
   },
+  variants: {
+    outline: {
+      select: {
+        border: '1px solid',
+      },
+    },
+    ghost: {
+      select: {
+        opacity: 0.5,
+      },
+    },
+    solid: {
+      select: {
+        bg: 'blue.100',
+      },
+    },
+  },
+  defaultProps: {
+    size: 'md',
+    variant: 'ghost',
+  },
 };
 
-const theme = extendTheme(customTheme);
+const theme = extendTheme({
+  components: {
+    ChakraSelect,
+  },
+})
 
-export default theme;
+export default theme
