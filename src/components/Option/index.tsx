51be3d781f68545a4090779b5d92e30673744e4c
Hussein AbdulQohar <aqmhussein@gmail.com>
2022-05-22 03:30:08 +0100

    feat: add option comp

--- src/components/Option/index.tsx
+++ src/components/Option/index.tsx
@@ -1,14 +1,26 @@
-import { Button, HTMLChakraProps } from "@chakra-ui/react";
+import { Button, HTMLChakraProps, useStyles } from "@chakra-ui/react";
 import { FC } from "react";
 
-interface OptionProps extends HTMLChakraProps<"option"> {
+interface OptionProps  {
   children?: string;
   label?: string;
+  selected?: boolean;
+  value: string;
+  onClick?: () => void;
+  onFocus?: () => void;
 }
 
-const Option: FC<OptionProps> = ({ label, children }) => {
+const Option: FC<OptionProps> = ({ label, selected, children, value, onClick, onFocus, ...props }) => {
+  const styles = useStyles();
   return (
-    <Button p={4} py={0} width="100%" textAlign={"left"} variant={"unstyled"}>
+    <Button
+      aria-selected={selected}
+      value={value}
+      onClick={onClick}
+      onFocus={onFocus}
+      __css={styles.option} 
+      {...props} 
+    >
       {label || children}
     </Button>
   );
